8b2d3caee18f425d665c85997a89f1dc01369736
yoshik <fujiwara.access@gmail.com>
2013-03-21 04:23:50 +0900

    時刻を表示するようにした

--- twitter_meteor.js
+++ twitter_meteor.js
@@ -4,6 +4,22 @@ if (M.isClient) {var T = Template;}
 
 var Tweets = new M.Collection("tweets");
 
+function relationTime (time) {
+  var ints = {second: 1,minute: 60,hour: 3600,day: 86400,week: 604800,month: 2592000,year: 31536000};
+  time = +new Date(time*1);
+  var gap = ((+new Date()) - time) / 1000,amount, measure;
+  if(gap<2){return 'just now'}
+  for (var i in ints) {if (gap > ints[i]) { measure = i; }}
+  amount = gap / ints[measure];
+  amount = gap > ints.day ? (Math.round(amount * 100) / 100) : Math.round(amount);
+  return Math.floor(amount) + ' ' + measure + (amount > 1 ? 's' : '') + ' ago';
+}
+function setTime(){
+  $('.time').each(function(){
+    $(this).text(relationTime($(this).attr('data-time')));
+  });
+}
+
 if (M.isClient) {
 
   T.loginout.events({
@@ -42,6 +58,8 @@ if (M.isClient) {
     var cursor = Tweets.find({},{sort:{'time':-1},limit:30});
     return cursor;
   };
+
+  setInterval("setTime()",1000);
 
 }
 
