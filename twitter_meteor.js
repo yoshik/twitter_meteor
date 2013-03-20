ea8379f1dd8dd2fd552f7711d2587f717f7f2bfd
yoshik <fujiwara.access@gmail.com>
2013-03-20 21:37:43 +0900

    Twitterのログイン・ログアウトボタンを設置

--- twitter_meteor.js
+++ twitter_meteor.js
@@ -1,21 +1,29 @@
+function log(err) {console.log(err);}
+
 var M = Meteor;
 if (M.isClient) {
   var T = Template;
-  T.hello.greeting = function () {
-    return "Welcome to twitter_meteor.";
-  };
-
-  T.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+  T.loginout.events({
+    'click #login' : function () {
+      M.loginWithTwitter(function (err) { log(err);});
+    }
+  });
+  T.loginout.events({
+    'click #logout' : function () {
+      M.logout(function (err) { log(err);});
     }
   });
 }
 
 if (M.isServer) {
   M.startup(function () {
-    // code to run on server at startup
+    var myConsumerKey = "xxxxxxxxxxxxxxxxxxxx";
+    var myConsumerSec = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+    Accounts.loginServiceConfiguration.insert({
+      service:     "twitter",
+      consumerKey: myConsumerKey,
+      secret:      myConsumerSec,
+      requestOfflineToken:true
+    });
   });
 }
